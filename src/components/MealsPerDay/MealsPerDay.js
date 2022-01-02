6f8eba123b444827babc6e33c45c3e3d6000393a
lucasmolenaar <lucasmolenaar@hotmail.com>
2022-01-02 11:24:56 +0100

    Created weekly mealplan page

---
+++ src/components/MealsPerDay/MealsPerDay.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './MealsPerDay.module.scss';
+import MealplanRecipeCard from "../MealplanRecipeCard/MealplanRecipeCard";
+
+function MealsPerDay({ day, info }) {
+
+    console.log(info)
+
+    return (
+        <div>
+            <h1 className={styles.title}>{day}</h1>
+
+            <ul className={styles['meals-per-day']}>
+                {info.meals.map(meal => {
+                    return (
+                        <MealplanRecipeCard
+                            title={meal.title}
+                            prepTime={meal.readyInMinutes}
+                            servings={meal.servings}
+                            urlLink={meal.sourceUrl}
+                        />
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default MealsPerDay;
