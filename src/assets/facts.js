c2461fee3f112c62e83d73172ab9257f6421b6bd
lucasmolenaar <lucasmolenaar@hotmail.com>
2022-01-05 09:59:20 +0100

    Added first facts

---
+++ src/assets/facts.js
@@ -0,0 +1,23 @@
+const facts = [
+    {
+        title: 'The most expensive pizza in the world costs $12.000',
+        content: 'That’s because it takes 72 hours to make, and it can only be made in your home by 3 Italian chefs. The pizza is topped with 3 types of caviar, bufala mozzarella, lobster from Norway and Cilento, and pink Australian sea salt.'
+    }, {
+        title: 'One fast-food burger can have meat from 100 different cows',
+        content: 'It sounds like a crazy amount, but the ground beef used to make burgers, both in fast food places and grocery stores, is made of a collection of muscle tissues.'
+    },
+    {
+        title: 'Crackers have holes in them for a reason',
+        content: 'During the baking process, if the crackers have holes in them, it prevents air bubbles from ruining the product.'
+    },
+    {
+        title: 'Ketchup used to be used as a medicine',
+        content: 'Back in the early 1800s, people thought tomatoes had medicinal qualities. One doctor claimed they could treat diarrhea and indigestion, so he made a recipe for a type of tomato ketchup which then became a pill.'
+    },
+    {
+        title: 'White chocolate isn’t chocolate',
+        content: 'Its name is deceiving because white chocolate doesn’t have any components of regular chocolate. It’s really just a mixture of sugar, milk, vanilla, lecithin, and cocoa butter.'
+    }
+];
+
+export default facts;
