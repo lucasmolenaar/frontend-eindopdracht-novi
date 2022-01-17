b48b4e8dd7ceb365c0727989ee7f1d4095d4c70d
lucasmolenaar <lucasmolenaar@hotmail.com>
2022-01-17 10:33:53 +0100

    Changed navbar for logged in users

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaUserAlt } from 'react-icons/fa';
 
 import styles from './NavBar.module.scss';
 
 const NavBar = () => {
+
+    const [isAuth, toggleIsAuth] = useState(true);
+    const [isHovering, toggleIsHovering] = useState(false);
+
     return (
+        !isAuth ?
         <div className={styles.NavBar}>
             <nav className={styles.navbar}>
                 <NavLink className={styles['logo-link']} exact to='/'>
@@ -27,6 +33,37 @@ const NavBar = () => {
                         <li><NavLink exact activeClassName={styles['active-nav-item']} className={styles['register-nav-item']} to="/register">Register</NavLink></li>
                         <li className={styles['login-nav-item']}><NavLink exact activeClassName={styles['active-nav-item']} to="/login">Login</NavLink></li>
                     </ul>
+                </div>
+            </nav>
+        </div>
+
+        :
+
+        <div className={styles.NavBar}>
+            <nav className={styles.navbar}>
+                <NavLink className={styles['logo-link']} exact to='/'>
+                    <div className={styles['logo-container']}>
+                        <h1 className={styles.logo}>
+                            Let's Cook
+                        </h1>
+                    </div>
+                </NavLink>
+
+                <ul className={styles['nav-items']}>
+                    <li><NavLink exact activeClassName={styles['active-nav-item']} className={styles['nav-item']} to="/">Search</NavLink></li>
+                    <li><NavLink exact activeClassName={styles['active-nav-item']} className={styles['nav-item']} to="/random">Random</NavLink></li>
+                    <li><NavLink exact activeClassName={styles['active-nav-item']} className={styles['nav-item']} to="/daily">Daily Mealplan</NavLink></li>
+                    <li><NavLink exact activeClassName={styles['active-nav-item']} className={styles['nav-item']} to="/weekly">Weekly Mealplan</NavLink></li>
+                </ul>
+
+                <div className={styles.account}
+                     onMouseOver={() => toggleIsHovering(true)}
+                     onMouseOut={() => toggleIsHovering(false)}
+                >
+                    <FaUserAlt />
+                    <span className={styles.username}>
+                        {isHovering ? <span onClick={() => toggleIsAuth(false)}>Log Out</span> : <span>gebruikersnaam</span>}
+                    </span>
                 </div>
             </nav>
         </div>
